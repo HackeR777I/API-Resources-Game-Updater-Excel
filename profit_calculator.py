dc869ed9a71e4b88510387384e733c822e8491cd
ZeVS556 <150081133+ZeVS556@users.noreply.github.com>
2026-03-14 12:23:04 +1200

    Update profit_calculator.py
    Исправлен недописанный код

--- profit_calculator.py
+++ profit_calculator.py
@@ -56,4 +56,63 @@ def build_daily_profit_report(
             {"player_price": 0.0, "system_price": 0.0}
         )
 
-        player_price = float(price_info.get("player_price", 0
+        player_price = float(price_info.get("player_price", 0.0))
+        system_price = float(price_info.get("system_price", 0.0))
+        effective_price = choose_effective_price(player_price, system_price)
+
+        income_day = 0.0
+        expense_day = 0.0
+        net_profit_day = 0.0
+
+        if balance_day > 0:
+            income_day = balance_day * effective_price
+            net_profit_day = income_day
+
+        elif balance_day < 0:
+            base_expense = abs(balance_day) * effective_price
+            expense_day = base_expense * (1 + logistics_percent)
+            net_profit_day = -expense_day
+
+        if group == "mine":
+            mine_income_day += income_day
+            mine_expense_day += expense_day
+            mine_net_profit_day += net_profit_day
+
+        elif group == "factory":
+            factory_income_day += income_day
+            factory_expense_day += expense_day
+            factory_net_profit_day += net_profit_day
+
+        profit_rows.append({
+            "resource_name": resource_name,
+            "group": group,
+            "balance_day": balance_day,
+            "player_price": player_price,
+            "system_price": system_price,
+            "effective_price": effective_price,
+            "income_day": income_day,
+            "expense_day": expense_day,
+            "net_profit_day": net_profit_day,
+        })
+
+    total_credits_cost_per_day = float(factory_stats.get("total_credits_cost_per_day", 0.0))
+
+    total_market_net_profit_day = mine_net_profit_day + factory_net_profit_day
+    total_net_profit_day = total_market_net_profit_day - total_credits_cost_per_day
+
+    return {
+        "profit_rows": profit_rows,
+        "logistics_percent": logistics_percent,
+
+        "mine_income_day": mine_income_day,
+        "mine_expense_day": mine_expense_day,
+        "mine_net_profit_day": mine_net_profit_day,
+
+        "factory_income_day": factory_income_day,
+        "factory_expense_day": factory_expense_day,
+        "factory_net_profit_day": factory_net_profit_day,
+
+        "total_market_net_profit_day": total_market_net_profit_day,
+        "total_credits_cost_per_day": total_credits_cost_per_day,
+        "total_net_profit_day": total_net_profit_day,
+    }
